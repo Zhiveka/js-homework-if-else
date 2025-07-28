c2dca7e628279cc63a9a0c48147eba433259dbf7
Zhiveka <zhibekmomun@gmail.com>
2025-07-28 16:15:19 +0600

    Initial commit: setup HTML and JS homework

---
+++ script.js
@@ -0,0 +1,17 @@
+console.log("Homework: if-else conditions");
+
+let testValues =[1, 0, -3];
+
+console.log("=== Task 1: a == 0 ===");
+testValues.forEach(a=> {
+    if (a === 0) {
+        console.log(a, "→ True");
+    } else {
+        console.log(a, "→ False");
+    }
+});
+
+
+
+
+// You'll continue like this for each condition
